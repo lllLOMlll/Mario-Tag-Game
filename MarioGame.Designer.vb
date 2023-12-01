d0bbab950a6c59e26beb75b2b8d2f36b1545be9a
Louis-Olivier Major <louis.olivier.major@gmail.com>
2023-12-01 15:06:18 -0500

    Initial commit

---
+++ MarioGame.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MarioGame
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(MarioGame))
+        Button_Stop = New Button()
+        PictureBox_Mario_Score = New PictureBox()
+        PictureBox_Luigi_Score = New PictureBox()
+        Label_Mario_Losses = New Label()
+        Label_Luigi_Losses = New Label()
+        Timer1 = New Timer(components)
+        Button_Start = New Button()
+        Button_New_Game = New Button()
+        Panel1 = New Panel()
+        Label_Title = New Label()
+        Label_Game_Lost = New Label()
+        CType(PictureBox_Mario_Score, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox_Luigi_Score, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button_Stop
+        ' 
+        Button_Stop.BackColor = Color.Lime
+        Button_Stop.FlatAppearance.BorderColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Button_Stop.FlatAppearance.BorderSize = 4
+        Button_Stop.FlatAppearance.CheckedBackColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Button_Stop.FlatStyle = FlatStyle.Flat
+        Button_Stop.Font = New Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button_Stop.Location = New Point(392, 155)
+        Button_Stop.Name = "Button_Stop"
+        Button_Stop.Size = New Size(158, 47)
+        Button_Stop.TabIndex = 3
+        Button_Stop.Text = "STOP"
+        Button_Stop.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox_Mario_Score
+        ' 
+        PictureBox_Mario_Score.Image = CType(resources.GetObject("PictureBox_Mario_Score.Image"), Image)
+        PictureBox_Mario_Score.Location = New Point(50, 78)
+        PictureBox_Mario_Score.Name = "PictureBox_Mario_Score"
+        PictureBox_Mario_Score.Size = New Size(73, 71)
+        PictureBox_Mario_Score.SizeMode = PictureBoxSizeMode.CenterImage
+        PictureBox_Mario_Score.TabIndex = 4
+        PictureBox_Mario_Score.TabStop = False
+        ' 
+        ' PictureBox_Luigi_Score
+        ' 
+        PictureBox_Luigi_Score.Image = CType(resources.GetObject("PictureBox_Luigi_Score.Image"), Image)
+        PictureBox_Luigi_Score.Location = New Point(477, 78)
+        PictureBox_Luigi_Score.Name = "PictureBox_Luigi_Score"
+        PictureBox_Luigi_Score.Size = New Size(73, 71)
+        PictureBox_Luigi_Score.SizeMode = PictureBoxSizeMode.CenterImage
+        PictureBox_Luigi_Score.TabIndex = 5
+        PictureBox_Luigi_Score.TabStop = False
+        ' 
+        ' Label_Mario_Losses
+        ' 
+        Label_Mario_Losses.AutoSize = True
+        Label_Mario_Losses.Font = New Font("Impact", 21.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_Mario_Losses.Location = New Point(139, 93)
+        Label_Mario_Losses.Name = "Label_Mario_Losses"
+        Label_Mario_Losses.Size = New Size(32, 36)
+        Label_Mario_Losses.TabIndex = 6
+        Label_Mario_Losses.Text = "0"
+        ' 
+        ' Label_Luigi_Losses
+        ' 
+        Label_Luigi_Losses.AutoSize = True
+        Label_Luigi_Losses.Font = New Font("Impact", 21.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_Luigi_Losses.Location = New Point(429, 93)
+        Label_Luigi_Losses.Name = "Label_Luigi_Losses"
+        Label_Luigi_Losses.Size = New Size(32, 36)
+        Label_Luigi_Losses.TabIndex = 7
+        Label_Luigi_Losses.Text = "0"
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' Button_Start
+        ' 
+        Button_Start.BackColor = Color.Lime
+        Button_Start.FlatAppearance.BorderColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Button_Start.FlatAppearance.BorderSize = 4
+        Button_Start.FlatAppearance.CheckedBackColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Button_Start.FlatStyle = FlatStyle.Flat
+        Button_Start.Font = New Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button_Start.Location = New Point(221, 155)
+        Button_Start.Name = "Button_Start"
+        Button_Start.Size = New Size(165, 47)
+        Button_Start.TabIndex = 2
+        Button_Start.Text = "START"
+        Button_Start.UseVisualStyleBackColor = False
+        ' 
+        ' Button_New_Game
+        ' 
+        Button_New_Game.BackColor = Color.Lime
+        Button_New_Game.FlatAppearance.BorderColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Button_New_Game.FlatAppearance.BorderSize = 4
+        Button_New_Game.FlatAppearance.CheckedBackColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        Button_New_Game.FlatStyle = FlatStyle.Flat
+        Button_New_Game.Font = New Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button_New_Game.Location = New Point(50, 155)
+        Button_New_Game.Name = "Button_New_Game"
+        Button_New_Game.Size = New Size(165, 47)
+        Button_New_Game.TabIndex = 1
+        Button_New_Game.Text = "NEW GAME"
+        Button_New_Game.UseVisualStyleBackColor = False
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(255))
+        Panel1.Location = New Point(50, 208)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(500, 350)
+        Panel1.TabIndex = 8
+        ' 
+        ' Label_Title
+        ' 
+        Label_Title.AutoSize = True
+        Label_Title.Font = New Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_Title.ForeColor = Color.Blue
+        Label_Title.Location = New Point(50, 20)
+        Label_Title.Name = "Label_Title"
+        Label_Title.Size = New Size(494, 40)
+        Label_Title.TabIndex = 9
+        Label_Title.Text = "Mario, Luigi and Boooooooooooo!"
+        ' 
+        ' Label_Game_Lost
+        ' 
+        Label_Game_Lost.AutoSize = True
+        Label_Game_Lost.Font = New Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label_Game_Lost.ForeColor = Color.Red
+        Label_Game_Lost.Location = New Point(211, 85)
+        Label_Game_Lost.Name = "Label_Game_Lost"
+        Label_Game_Lost.Size = New Size(175, 40)
+        Label_Game_Lost.TabIndex = 10
+        Label_Game_Lost.Text = "Games Lost"
+        ' 
+        ' MarioGame
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(255))
+        ClientSize = New Size(585, 570)
+        Controls.Add(Label_Game_Lost)
+        Controls.Add(Label_Title)
+        Controls.Add(Label_Luigi_Losses)
+        Controls.Add(Label_Mario_Losses)
+        Controls.Add(PictureBox_Luigi_Score)
+        Controls.Add(PictureBox_Mario_Score)
+        Controls.Add(Button_Stop)
+        Controls.Add(Button_Start)
+        Controls.Add(Button_New_Game)
+        Controls.Add(Panel1)
+        Name = "MarioGame"
+        Text = "MarioGame"
+        CType(PictureBox_Mario_Score, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox_Luigi_Score, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Button_Stop As Button
+    Friend WithEvents PictureBox_Mario_Score As PictureBox
+    Friend WithEvents PictureBox_Luigi_Score As PictureBox
+    Friend WithEvents Label_Mario_Losses As Label
+    Friend WithEvents Label_Luigi_Losses As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button_Start As Button
+    Friend WithEvents Button_New_Game As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label_Title As Label
+    Friend WithEvents Label_Game_Lost As Label
+End Class
---
+++ MarioGame.vb
@@ -0,0 +1,184 @@
+﻿Imports System.Runtime.Intrinsics.Arm
+
+Public Class MarioGame
+    Inherits Form
+
+    Private Mario, Luigi, Boo As Characters
+    Private MarioLosses As Integer = 0
+    Private LuigiLosses As Integer = 0
+    Private movement() As String = {"left", "right", "up", "down"}
+    Private WithEvents GameTimer As New Timer()
+    Shared borderWidth As Integer = 5
+    Private gameOver As Boolean = False
+
+
+
+    Public Sub New()
+        ' Initialize components
+        InitializeComponent()
+
+        ' Initialize characters
+        InitializeCharacters()
+
+        ' Configure Timer
+        ConfigureTimer()
+    End Sub
+
+    Private Sub InitializeCharacters()
+        ' Initialize characters with images, sizes, and starting positions
+        Mario = New Characters("Mario", New Point(30, 100), New Size(30, 30), My.Resources.mario)
+        Luigi = New Characters("Luigi", New Point(30, 200), New Size(30, 30), My.Resources.luigi)
+        Boo = New Characters("Boo", New Point(450, 150), New Size(30, 30), My.Resources.boo)
+
+        ' Add characters to the Panel
+        Panel1.Controls.Add(Mario)
+        Panel1.Controls.Add(Luigi)
+        Panel1.Controls.Add(Boo)
+
+        ' Set the location relative to the Panel
+        Mario.Location = New Point(30, 100)
+        Luigi.Location = New Point(30, 200)
+        Boo.Location = New Point(450, 150)
+    End Sub
+
+
+    Private Sub ConfigureTimer()
+        Timer1.Interval = 200 ' Set the timer interval (in milliseconds)
+        AddHandler Timer1.Tick, AddressOf GameTick
+    End Sub
+
+    Private Sub GameTick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' Because I dont want the tick to continue if it's game over
+        ' I had a problem. 2 looses instead of 1
+        If gameOver Then Return
+        Randomize()
+        Dim r As Integer
+
+        ' Adjust the boundary limits to be within the Panel and account for the border
+        Dim borderWidth As Integer = 5
+        Dim boundaryX As Integer = Panel1.Width - Mario.Width
+        Dim boundaryY As Integer = Panel1.Height - Mario.Height
+
+        r = CInt(Math.Floor(Rnd() * movement.Length))
+        Boo.MoveCharacter(movement(r), boundaryX, boundaryY)
+
+        r = CInt(Math.Floor(Rnd() * movement.Length))
+        Mario.MoveCharacter(movement(r), boundaryX, boundaryY)
+
+        r = CInt(Math.Floor(Rnd() * movement.Length))
+        Luigi.MoveCharacter(movement(r), boundaryX, boundaryY)
+
+        CheckCollisions()
+    End Sub
+
+
+    Private Sub CheckCollisions()
+        If Boo.Bounds.IntersectsWith(Mario.Bounds) Then
+            Timer1.Stop()
+            gameOver = True
+            MarioLosses += 1
+            Label_Mario_Losses.Text = MarioLosses
+            MsgBox("Mario loses!", MsgBoxStyle.Information, "Game Over")
+            Return
+        End If
+
+        If Boo.Bounds.IntersectsWith(Luigi.Bounds) Then
+            Timer1.Stop()
+            gameOver = True
+            LuigiLosses += 1
+            Label_Luigi_Losses.Text = LuigiLosses
+            MsgBox("Luigi loses!", MsgBoxStyle.Information, "Game Over")
+            Return
+        End If
+    End Sub
+
+
+
+    ' Start Game Button
+    Private Sub Button_Start_Click(sender As Object, e As EventArgs) Handles Button_Start.Click
+        Timer1.Start()
+    End Sub
+
+    ' Stop Game Button
+    Private Sub Button_Stop_Click(sender As Object, e As EventArgs) Handles Button_Stop.Click
+        Timer1.Stop()
+    End Sub
+
+    ' New Game Button
+    Private Sub Button_New_Game_Click(sender As Object, e As EventArgs) Handles Button_New_Game.Click
+        'Reset the game over boolean
+        gameOver = False
+        'Clear the panel
+        Panel1.Controls.Clear()
+        'Add new characters
+        InitializeCharacters()
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+        Dim borderWidth As Integer = 5
+        Dim borderColor As Color = Color.Red
+
+        ' Create a pen with the desired border color and width
+        Using borderPen As New Pen(borderColor, borderWidth)
+            ' Adjust the rectangle to fit the border inside the panel
+            Dim rect As New Rectangle(0, 0, Panel1.Width - borderWidth, Panel1.Height - borderWidth)
+
+            ' Draw the rectangle border
+            e.Graphics.DrawRectangle(borderPen, rect)
+        End Using
+    End Sub
+
+
+    ' Characters class provided by your teacher
+
+    Public Class Characters : Inherits Label
+        Dim position As Point
+        'constructor
+        Public Sub New(ByVal name As String, ByVal location As Point, ByVal size As Size, ByVal face As Image)
+            Me.Name = name
+            Me.Size = size
+            Me.Location = location
+            Me.position = location
+            Me.BackgroundImage = face
+        End Sub
+        ''' <summary>
+        ''' get boundry and direction to change position (in form, location changes 
+        ''' </summary>
+        ''' <param name="direction"></param>
+        ''' <param name="boundryX"></param>
+        ''' <param name="boundryY"></param>
+        Public Sub MoveCharacter(ByVal direction, ByVal boundaryX, ByVal boundaryY)
+            Me.position = Me.Location ' Current position
+
+            ' Calculate new position based on the direction
+            Select Case direction
+                Case "right"
+                    If Me.position.X + Me.Size.Width + borderWidth <= boundaryX Then
+                        Me.position.X += Me.Size.Width
+                    End If
+                Case "left"
+                    If Me.position.X - Me.Size.Width >= borderWidth Then
+                        Me.position.X -= Me.Size.Width
+                    End If
+                Case "up"
+                    If Me.position.Y - Me.Size.Height >= borderWidth Then
+                        Me.position.Y -= Me.Size.Height
+                    End If
+                Case "down"
+                    If Me.position.Y + Me.Size.Height + borderWidth <= boundaryY Then
+                        Me.position.Y += Me.Size.Height
+                    End If
+            End Select
+
+            ' Update the location only if it's within the boundaries
+            Me.Location = Me.position
+        End Sub
+
+
+
+
+    End Class
+
+End Class
+
+
